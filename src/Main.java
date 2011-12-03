cfe79084a037b580ccfbb6e2da747903c358cae8
Ryan Colombo <ryco218@yahoo.com>
2011-12-03 18:45:38 -0500

    committed main

--- src/Main.java
+++ src/Main.java
@@ -28,23 +28,77 @@ public class Main {
 		
 		Mux aluSrcMux = new Mux();
 		Mux memToRegMux = new Mux();
-		Mux regFileMux = new Mux();
+		Mux regDstMux = new Mux();
+		Mux jumpMux = new Mux();
+		Mux branchMux = new Mux();
+		ShiftLeftTwo slt1 = new ShiftLeftTwo();
+		ShiftLeftTwo slt2 = new ShiftLeftTwo();
 		ALUControl aluControl = new ALUControl();
 		ALU alu = new ALU();
+		ALU aluAdd = new ALU();
+		aluAdd.control.setValue((long)2);
 		MemoryIO memoryIo = new MemoryIO(dataMemory);
 		Control control = new Control();
 		RegisterFile regFile = new RegisterFile();
 		Fetch fetch = new Fetch(instrMemory);
+		And branchMuxAnd = new And();
+		
 		aluSrcMux.output.connectTo(alu.input2);
 		aluControl.aluControl.connectTo(alu.control);
 		alu.result.connectTo(memoryIo.address);
 		alu.result.connectTo(memToRegMux.input0);
+		alu.zero.connectTo(branchMuxAnd.input1);
 		memoryIo.readData.connectTo(memToRegMux.input1);
 		memToRegMux.output.connectTo(regFile.writeData);
-		regFileMux.output.connectTo(regFile.writeReg);
+		regDstMux.output.connectTo(regFile.writeReg);
+		slt1.out.connectTo(jumpMux.input1);
+		slt2.out.connectTo(aluAdd.input2);
+		branchMuxAnd.output.connectTo(branchMux.switcher);
+		jumpMux.output.connectTo(fetch.PC);
+		
+		// connect the control signals
+		control.regDst.connectTo(regDstMux.switcher);
+		control.jump.connectTo(jumpMux.switcher);
+		control.branch.connectTo(branchMuxAnd.input0);
+		control.memRead.connectTo(memoryIo.memRead);
+		control.memToReg.connectTo(memToRegMux.input1);
+		control.aluOp.connectTo(aluControl.aluOp);
+		control.memWrite.connectTo(memoryIo.memWrite);
+		control.aluSrc.connectTo(aluSrcMux.switcher);
+		control.regWrite.connectTo(regFile.regWrite);
+		
+		regFile.readReg1.connectTo(alu.input1);
+		regFile.readReg2.connectTo(aluSrcMux.input0);
+		regFile.readReg2.connectTo(memoryIo.writeData);
 		
 		for(Long instruction : instructions){
+			// fetch instruction and increment PC
 			fetch.clockEdge();
+			// connect PC to Add ALU
+			fetch.PC.connectTo(aluAdd.input1);
+			
+			// create decode object with this instruction
+			Decode decode = new Decode(fetch.getInstruction());
+			// make the necessary connections
+			decode.opcode.connectTo(control.opcode);
+			decode.rs.connectTo(regFile.readReg1);
+			decode.rt.connectTo(regFile.readReg2);
+			decode.rt.connectTo(regDstMux.input0);
+			decode.rd.connectTo(regDstMux.input1);
+			decode.target.connectTo(slt1.in);
+			decode.offset.connectTo(slt2.in);
+			decode.funct.connectTo(aluControl.func);
+			// decode the instruction
+			decode.clockEdge();
+			
+			// set the output control signals
+			control.setSignals();
+			
+			// clock the mux that takes regDST as switcher
+			regDstMux.clockEdge();
+			
+			// clock the regfile
+			regFile.clockEdge();
 		}
 	}
 
