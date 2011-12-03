d971f65ae05090d638f05d6b370990a0068ec4a6
Ryan Colombo <ryco218@yahoo.com>
2011-12-03 17:28:37 -0500

    some updates to main

--- src/Main.java
+++ src/Main.java
@@ -28,16 +28,24 @@ public class Main {
 		
 		Mux aluSrcMux = new Mux();
 		Mux memToRegMux = new Mux();
+		Mux regFileMux = new Mux();
 		ALUControl aluControl = new ALUControl();
 		ALU alu = new ALU();
 		MemoryIO memoryIo = new MemoryIO(dataMemory);
-		
+		Control control = new Control();
+		RegisterFile regFile = new RegisterFile();
+		Fetch fetch = new Fetch(instrMemory);
 		aluSrcMux.output.connectTo(alu.input2);
 		aluControl.aluControl.connectTo(alu.control);
 		alu.result.connectTo(memoryIo.address);
 		alu.result.connectTo(memToRegMux.input0);
 		memoryIo.readData.connectTo(memToRegMux.input1);
+		memToRegMux.output.connectTo(regFile.writeData);
+		regFileMux.output.connectTo(regFile.writeReg);
 		
+		for(Long instruction : instructions){
+			fetch.clockEdge();
+		}
 	}
 
 }
