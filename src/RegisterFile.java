173e843d37e0f3e01b5596d3e779e42a45cf38ca
Ryan Colombo <ryco218@yahoo.com>
2011-12-03 17:27:02 -0500

    register file

--- src/RegisterFile.java
+++ src/RegisterFile.java
@@ -2,6 +2,14 @@ public class RegisterFile implements Clockable{
 	
 	public Register[] regFile;
 	
+	public Pin writeReg = new Pin();
+	
+	public Pin readReg1 = new Pin();
+	
+	public Pin readReg2 = new Pin();
+	
+	public Pin writeData = new Pin();
+	
 	public RegisterFile(){
 		
 		// initializes with 32 words of memory set to 0
@@ -11,18 +19,34 @@ public class RegisterFile implements Clockable{
 		}
 	}
 	
+	public void setWriteReg(long val){
+		writeReg.setValue(val);
+	}
+	
+	public void setRegReg1(long val){
+		readReg1.setValue(val);
+	}
+	
+	public void setReadReg2(long val){
+		readReg2.setValue(val);
+	}
+	
+	public void setWriteData(long val){
+		writeData.setValue(val);
+	}
+	
 	public long getVal(int index){
 		return regFile[index].getValue();
 	}
 	
 	public void update(int index, long val){
-		if(index >=0 && index < 32){
+		if(index >=1 && index < 32){
 			regFile[index].setValue(val);
 		}
 	}
 	
 	public void clockEdge(){
-		for(int x=0; x<32; x++){
+		for(int x=1; x<32; x++){
 			regFile[x].update();
 		}
 	}
