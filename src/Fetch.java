88a88631bead9c553d1d9221dd942f4ef7c2144e
Ryan Colombo <ryco218@yahoo.com>
2011-12-03 15:45:47 -0500

    changed type of instructionSet, added a setInstruction method

--- src/Fetch.java
+++ src/Fetch.java
@@ -1,29 +1,29 @@
-import java.util.*;
-
 public class Fetch implements Clockable{
 	
 	//index of the program counter
 	public long PC;
 	
+	// the complete set of instructions
+	public Memory instructionSet;
+		
 	// the instruction to fetch
 	public long instruction;
 	
 	// initialize program counter to zero
-	public Fetch(){
+	public Fetch(Memory instructionSet){
+		this.instructionSet = instructionSet;
 		PC = 0;
 	}
 	
-	// fetch the instruction from instruction memory
-	public void doFetch(HashMap<Long,Long> mem){	
-		instruction = mem.get(PC);
-		PC++;
+	public void setInstruction(){
+		instruction = instructionSet.loadWord(PC);
 	}
-	
 	public long getInstruction(){
 		return instruction;
 	}
 	
 	public void clockEdge(){
-		// Todo
+		setInstruction();
+		PC++;
 	}
 }
