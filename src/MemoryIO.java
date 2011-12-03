c22adf2689fab3045434f21db83eab83de97fa1f
Eliott Wiener <eliottwiener@gmail.com>
2011-12-03 14:16:29 -0500

    add clockEdge body

--- src/MemoryIO.java
+++ src/MemoryIO.java
@@ -5,11 +5,20 @@ public class MemoryIO implements Clockable {
 	public final Pin data = new Pin();
 	public final Pin memWrite = new Pin();
 	public final Pin memRead = new Pin();
+	public final Memory mem;
+	
+	
+	public MemoryIO(final Memory dataMemory){
+		this.mem = dataMemory;
+	}
 	
 	@Override
 	public void clockEdge() {
-		// TODO Auto-generated method stub
-
+		if(memRead.getValue().equals(1)){
+			data.setValue(mem.loadWord(address.getValue()));
+		} else if(memWrite.getValue().equals(1)){
+			mem.storeWord(address.getValue(), data.getValue());
+		}
 	}
 
 }
