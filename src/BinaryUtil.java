63babfc79321dadd5a47bf94a5add8323ee571c7
Ryan Colombo <ryco218@yahoo.com>
2011-12-04 21:20:40 -0500

    Changed the padding from loop to String function

--- src/BinaryUtil.java
+++ src/BinaryUtil.java
@@ -1,16 +1,8 @@
 
 public class BinaryUtil {
 	public static final String pad(String s){
-		int len = s.length();
-		if(len == 32){
-			return s;
-		} else if(len > 32){
+		if(s.length() > 32){
 			throw new RuntimeException("Attempt to pad out long string that is too many bits: " + s);
-		} else {
-			for(int z = 32 - len ; z > 0 ; z--){
-				s = "0" + s;
-			}
-			return s;
-		}
+		} else return String.format("%32s", s).replace(" ", "0");
 	}
 }
