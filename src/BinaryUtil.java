af9a75414d6515499540e045066dbc4068ab197a
Ryan Colombo <ryco218@yahoo.com>
2011-12-05 18:35:14 -0500

    removed a line

--- src/BinaryUtil.java
+++ src/BinaryUtil.java
@@ -1,9 +1,10 @@
-
 public class BinaryUtil {
 	public static final String pad(String s, int numBytes){
 		if(s.length() > numBytes){
 			throw new RuntimeException("Attempt to pad out long string that is too many bits: " + s);
-		} else return String.format("%" + numBytes + "s", s).replace(" ", "0");
+		} else {
+			return String.format("%" + numBytes + "s", s).replace(" ", "0");
+		}
 	}
 	
 }
