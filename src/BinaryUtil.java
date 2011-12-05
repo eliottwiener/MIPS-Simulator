d895f403abea867952a274e8de155b7922204a00
Ryan Colombo <ryco218@yahoo.com>
2011-12-05 15:40:23 -0500

    Changed BinaryUtil so there is only one pad function which takes a string and numBytes. String gets padded to the length specified by numBytes

--- src/BinaryUtil.java
+++ src/BinaryUtil.java
@@ -1,15 +1,9 @@
 
 public class BinaryUtil {
-	public static final String pad(String s){
-		if(s.length() > 32){
+	public static final String pad(String s, int numBytes){
+		if(s.length() > numBytes){
 			throw new RuntimeException("Attempt to pad out long string that is too many bits: " + s);
-		} else return String.format("%32s", s).replace(" ", "0");
+		} else return String.format("%" + numBytes + "s", s).replace(" ", "0");
 	}
 	
-	public static final String pad8(String s){
-		if(s.length() > 8){
-			throw new RuntimeException("Attempt to pad out String that is greater than 8 bits: " + s);
-		} else return String.format("%8s", s).replace(" ", "0");
-		
-	}
 }
