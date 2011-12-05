1120ee66bf458ca83c9effaf331702ae2871aa04
Ryan Colombo <ryco218@yahoo.com>
2011-12-05 16:51:37 -0500

    Added debug statements

--- src/Combiner.java
+++ src/Combiner.java
@@ -11,6 +11,11 @@ public class Combiner implements Clockable{
 		String ja250 = BinaryUtil.pad(Long.toBinaryString(jumpAddr.getValue()),32).substring(6);
 		String outString = pcinc3128 + ja250;
 		out.setValue(Long.parseLong(outString,2));
+		
+		System.out.println("[DEBUG] Class:Combiner" +
+						  "\npcIn:" + BinaryUtil.pad(Long.toBinaryString(pcIn.getValue()), 32) +
+						  "\njumpAddr:" + BinaryUtil.pad(Long.toBinaryString(jumpAddr.getValue()), 32) +
+						  "\nout:" + BinaryUtil.pad(Long.toBinaryString(out.getValue()), 32));
 	}
 	
 	
