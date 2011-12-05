8f9febe52b1ce20e97d86bf07363c3991bad3883
Ryan Colombo <ryco218@yahoo.com>
2011-12-05 12:39:02 -0500

    added pad8 function

--- src/BinaryUtil.java
+++ src/BinaryUtil.java
@@ -5,4 +5,11 @@ public class BinaryUtil {
 			throw new RuntimeException("Attempt to pad out long string that is too many bits: " + s);
 		} else return String.format("%32s", s).replace(" ", "0");
 	}
+	
+	public static final String pad8(String s){
+		if(s.length() > 8){
+			throw new RuntimeException("Attempt to pad out String that is greater than 8 bits: " + s);
+		} else return String.format("$8s", s).replace(" ", "0");
+		
+	}
 }
