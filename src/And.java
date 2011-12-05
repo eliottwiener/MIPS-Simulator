d74b44357fb11ba7f00a9245f97a09997b10db19
Ryan Colombo <ryco218@yahoo.com>
2011-12-05 16:50:18 -0500

    Added debug statements

--- src/And.java
+++ src/And.java
@@ -8,13 +8,17 @@ public class And implements Clockable {
 	
 	And(){}
 	
-	@Override
 	public void clockEdge() {
 		if(input0.getValue().equals(1) && input1.getValue().equals(1)){
 			output.setValue((long)1);
 		} else {
 			output.setValue((long)0);
 		}
+		
+		System.out.println("[DEBUG] Class:And" +
+						  "\noutput:" + BinaryUtil.pad(Long.toBinaryString(output.getValue()), 32) +
+				          "\ninput0:" + BinaryUtil.pad(Long.toBinaryString(output.getValue()), 32) +
+						  "\ninput1:" + BinaryUtil.pad(Long.toBinaryString(output.getValue()), 32));
 	}
 
 }
