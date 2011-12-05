add20898709daf0e613f8803eb350293298e85e5
Ryan Colombo <ryco218@yahoo.com>
2011-12-05 00:06:31 -0500

    Added a combiner, made necessary changes to ALU, PC, and decode objects to reflect the use of splitter. Filled out some more of the loop, added a debug print statement.

--- src/Main.java
+++ src/Main.java
@@ -16,11 +16,12 @@ public class Main {
 		//load instructions into instruction memory
 		Long numInstr = (long) instructions.size();
 		Long firstInstrAddr = Long.parseLong("1000", 16);
-		Memory instrMemory = new Memory(firstInstrAddr,firstInstrAddr+(numInstr*4-1));
+		Memory instrMemory = new Memory(firstInstrAddr,firstInstrAddr+(numInstr*4)-1);
 
 		for(int i = 0; i < numInstr ; i++){
 			instrMemory.storeWord(firstInstrAddr+i*4, instructions.get(i));
 		}
+		
 		
 		//initialize data memory
 		Memory dataMemory = new Memory(Long.parseLong("0", 16),Long.parseLong("ffc", 16));
@@ -34,10 +35,10 @@ public class Main {
 		ShiftLeftTwo slt1 = new ShiftLeftTwo();
 		ShiftLeftTwo slt2 = new ShiftLeftTwo();
 		ALUControl aluControl = new ALUControl();
-		ALU alu = new ALU();
-		ALU aluAdd = new ALU();
+		ALU alu = new ALU(2);
+		ALU aluAdd = new ALU(1);
 		aluAdd.control.setValue((long)2);
-		ALU aluP4 = new ALU();
+		ALU aluP4 = new ALU(3);
 		aluP4.control.setValue((long)2);
 		aluP4.input2.setValue((long) 4);
 		MemoryIO memoryIo = new MemoryIO(dataMemory);
@@ -47,9 +48,11 @@ public class Main {
 		And branchMuxAnd = new And();
 		ProgramCounter pc = new ProgramCounter();
 		Decode decode = new Decode();
+		Combiner combiner = new Combiner();
 		
 		// Connect output of PC
-		pc.pcOut.connectTo(fetch.pc);
+		pc.pcOut.connectToList[0].connectTo(fetch.pc);
+		pc.pcOut.connectToList[1].connectTo(aluP4.input1);
 		
 		// Connect output of fetch
 		fetch.instr.connectTo(decode.instruction);
@@ -57,8 +60,8 @@ public class Main {
 		// Connect the outputs of the decode
 		decode.opcode.connectTo(control.opcode);
 		decode.rs.connectTo(regFile.readReg1);
-		decode.rt.connectTo(regFile.readReg2);
-		decode.rt.connectTo(regDstMux.input0);
+		decode.rt.connectToList[0].connectTo(regFile.readReg2);
+		decode.rt.connectToList[1].connectTo(regDstMux.input0);
 		decode.rd.connectTo(regDstMux.input1);
 		decode.target.connectTo(slt1.in);
 		decode.offset.connectTo(slt2.in);
@@ -71,19 +74,24 @@ public class Main {
 		
 		// Connect outputs of ALU and related
 		aluSrcMux.output.connectTo(alu.input2);
-		alu.result.connectTo(memoryIo.address);
-		alu.result.connectTo(memToRegMux.input0);
+		alu.result.connectToList[0].connectTo(memoryIo.address);
+		alu.result.connectToList[1].connectTo(memToRegMux.input0);
 		alu.zero.connectTo(branchMuxAnd.input1);
+		aluP4.result.connectToList[0].connectTo(aluAdd.input1);
+		aluP4.result.connectToList[1].connectTo(branchMux.input0);
+		aluP4.result.connectToList[2].connectTo(combiner.pcIn);
+		aluAdd.result.connectToList[0].connectTo(branchMux.input0);
 		
 		// Connect output of memoryIo
 		memoryIo.readData.connectTo(memToRegMux.input1);
 		
 		memToRegMux.output.connectTo(regFile.writeData);
 		regDstMux.output.connectTo(regFile.writeReg);
-		slt1.out.connectTo(jumpMux.input1);
+		slt1.out.connectTo(combiner.jumpAddr);
 		slt2.out.connectTo(aluAdd.input2);
 		branchMuxAnd.output.connectTo(branchMux.switcher);
 		jumpMux.output.connectTo(pc.pcIn);
+		combiner.out.connectTo(jumpMux.input1);
 		
 		// connect the control signals
 		control.regDst.connectTo(regDstMux.switcher);
@@ -99,24 +107,60 @@ public class Main {
 		
 		pc.pcIn.setValue(firstInstrAddr);
 		for(Long instruction : instructions){
+			// send PC to fetch object
 			pc.clockEdge();
+
 			// fetch the instruction
 			fetch.clockEdge();
-			
+			System.out.println("[DEBUG] This Instruction is: " + fetch.instr.getValue());
 			// decode the instruction
 			decode.clockEdge();
 			if(decode.isHalt()){
 				break;
 			}
 			
+			// clock the SLTs
+			slt1.clockEdge();
+			slt2.clockEdge();
+			
+			// clock the Add ALU
+			aluAdd.clockEdge();
+			
 			// set the output control signals
 			control.setSignals();
 			
-			// clock the mux that takes regDST as switcher
+			// set ALU control signals
+			aluControl.update();
+			
+			// clock the RegDST Mux
 			regDstMux.clockEdge();
 			
 			// clock the regfile
 			regFile.clockEdge();
+			
+			// clock the ALUSrc Mux
+			aluSrcMux.clockEdge();
+			
+			// clock the ALU
+			alu.clockEdge();
+			
+			// clock the AND
+			branchMuxAnd.clockEdge();
+			
+			// clock the Memory IO
+			memoryIo.clockEdge();
+			
+			// clock the combiner
+			combiner.clockEdge();
+			
+			// clock the Branch Mux
+			branchMux.clockEdge();
+			
+			// clock the memToRegMux
+			memToRegMux.clockEdge();
+			
+			// clock the jump Mux
+			jumpMux.clockEdge();
 		}
 		System.out.println("Final register values:");
 		for(int i = 0 ; i < 32 ; i++){
