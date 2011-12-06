e613a0ab44cf2a638faf0346eabeb5e82fb3be8e
Ryan Colombo <ryco218@yahoo.com>
2011-12-06 18:53:13 -0500

    got rid of debug statements

--- src/MemoryIO.java
+++ src/MemoryIO.java
@@ -14,18 +14,11 @@ public class MemoryIO implements Clockable {
 	}
 	
 	public void clockEdge() {
-		System.out.println("[DEBUG] Class: MemoryIO");
 		if(memRead.getValue().equals((long)1)){
 			readData.setValue(mem.loadWord(address.getValue()));
-			System.out.println("readData:" + BinaryUtil.pad(Long.toBinaryString(readData.getValue()), 32));
 		} else if(memWrite.getValue().equals(1)){
 			mem.storeWord(address.getValue(), writeData.getValue());
 		}
-		
-		System.out.println("address:" + BinaryUtil.pad(Long.toBinaryString(address.getValue()), 32) +
-				         "\nwriteData:" + BinaryUtil.pad(Long.toBinaryString(writeData.getValue()), 32) +
-				         "\nmemWrite:" + BinaryUtil.pad(Long.toBinaryString(memWrite.getValue()), 32) +
-						 "\nmemRead:" + BinaryUtil.pad(Long.toBinaryString(memRead.getValue()), 32));
 	}
 
 }
