7f384ff77d3f7be19d54ef15856514978abc714d
Ryan Colombo <ryco218@yahoo.com>
2011-12-05 20:11:46 -0500

    SLT

--- src/ShiftLeftTwo.java
+++ src/ShiftLeftTwo.java
@@ -1,12 +1,15 @@
-
 public class ShiftLeftTwo implements Clockable{
 	Pin in = new Pin();
 	Pin out = new Pin();
+	int outputBits;
 	
-	ShiftLeftTwo(){}
+	ShiftLeftTwo(int outputBits){
+		this.outputBits = outputBits;
+	}
 
 	public void clockEdge() {
-		out.setValue(in.getValue()<<2);
+		String myStr = BinaryUtil.pad(Long.toBinaryString(in.getValue()), outputBits);
+	    out.setValue(Long.parseLong(myStr.substring(2,outputBits-1) + "00", 2));
 		System.out.println("[DEBUG] Class: ShiftLeftTwo" + 
 						  "\nin:" + BinaryUtil.pad(Long.toBinaryString(in.getValue()), 32) +
 						  "\nout:" + BinaryUtil.pad(Long.toBinaryString(out.getValue()), 32));
