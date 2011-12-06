03993827951cfe0389638eb799694c0db07cac9b
Ryan Colombo <ryco218@yahoo.com>
2011-12-06 18:49:42 -0500

    got rid of debug statements

--- src/Mux.java
+++ src/Mux.java
@@ -14,17 +14,10 @@ public class Mux implements Clockable{
 	Mux(){}
 	
 	public void clockEdge() {
-		System.out.println("[DEBUG] Class:Mux");
-		System.out.println("switcher:" + BinaryUtil.pad(Long.toBinaryString(switcher.getValue()), 32));
 		if(switcher.getValue().equals((long)0)){
 			output.setValue(input0.getValue());
-			System.out.println("input0:" + BinaryUtil.pad(Long.toBinaryString(input0.getValue()), 32));
 		} else {
 			output.setValue(input1.getValue());
-			System.out.println("input1:" + BinaryUtil.pad(Long.toBinaryString(input1.getValue()), 32));			
-		}
-		
-		
-		System.out.println("\noutput:" + BinaryUtil.pad(Long.toBinaryString(output.getValue()), 32));
+		}		
 	}
 }
