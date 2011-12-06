71251a0b2cd9054342e54351a16c4b4e816ee7cb
Ryan Colombo <ryco218@yahoo.com>
2011-12-05 20:10:23 -0500

    Added debug

--- src/ProgramCounter.java
+++ src/ProgramCounter.java
@@ -6,6 +6,9 @@ public class ProgramCounter implements Clockable{
 	public ProgramCounter(){}
 
 	public void clockEdge() {
+		System.out.println("[DEBUG] Class:ProgramCounter" + 
+				   "\npcIn:" + BinaryUtil.pad(Long.toBinaryString(pcIn.getValue()),32) +
+				   "\npcOut:" + pcOut.getValue());
 		pcOut.setValue(pcIn.getValue());
 	}
 }
