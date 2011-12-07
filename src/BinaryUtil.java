cde32ad1acf229622e78b166ab344ea62fdd8d90
Ryan Colombo <ryco218@yahoo.com>
2011-12-06 21:10:24 -0500

    Removed sign extend from binary util and made it its own class

--- src/BinaryUtil.java
+++ src/BinaryUtil.java
@@ -11,15 +11,4 @@ public class BinaryUtil {
 			return String.format("%" + numBytes + "s", s).replace(" ", "0");
 		}
 	}
-	
-	public static final String signExtend(String s, int numBytes){
-		if(s.length() > numBytes){
-			throw new RuntimeException("Attempt to sign-extend long string that is too many bits: " + s);
-		}else {
-			// pads with the most significant bit
-			String msb=s.substring(0, 1);
-			return String.format("%" + numBytes + "s", s).replace(" ", msb);
-		}
-	}
-	
 }
