f15bff0cb8c1ac7f1a1930e053c192ea371245e7
Ryan Colombo <ryco218@yahoo.com>
2011-12-06 20:11:56 -0500

    Adding Register back

---
+++ src/Register.java
@@ -0,0 +1,17 @@
+public class Register{
+
+	private long value;
+	
+	Register(long value) {
+		this.value = value;
+	}
+
+	public long getValue(){
+		return value;
+	}
+
+	public void setValue(long v){
+		value = v;
+	}
+
+}
