f608e29ac6bc0e71161f8cf9368104422dd582cf
Ryan Colombo <ryco218@yahoo.com>
2011-12-09 18:37:54 -0500

    RegFile

--- src/RegisterFile.java
+++ src/RegisterFile.java
@@ -15,7 +15,7 @@ public class RegisterFile implements Clockable{
 		// initializes with 32 words of memory set to 0
 		regFile = new Register[32];
 		for(int x=0; x<32; x++){
-			regFile[x] = new Register(new BinaryNum("0"));
+			regFile[x] = new Register(new BinaryNum("0").pad(32));
 		}
 	}
 	
@@ -27,10 +27,11 @@ public class RegisterFile implements Clockable{
 	// updates value at register $'index' with 'val'
 	public void setRegister(int index, BinaryNum val){
 		if(index >=1 && index < 32){
+			// If value = 0x886, make it 0x8886
 			if(index == 12 && val.toString().equals("100010000110")){
 				val = new BinaryNum("1000100001101000100010000110");
 			}
-			regFile[index].setValue(val);
+			regFile[index].setValue(val.pad(32));
 		}else if(index != 0){
 			throw new Error("Cannot access $r" + index);
 		}
@@ -40,15 +41,12 @@ public class RegisterFile implements Clockable{
 		// if regWrite is 1, then we write to the register
 		// which is stored in writeReg. Data is stored in writeData;
 		if(regWrite.getValue() != null && regWrite.getValue().equals(new BinaryNum("1"))){
-			setRegister(Integer.parseInt(writeReg.getValue().toString(),2),
-					writeData.getValue());
+			setRegister(writeReg.getValue().toLong().intValue(), writeData.getValue());
 			writeReg.setValue(new BinaryNum("0"));
 		}
 		
-		readData1.setValue(getVal(Integer.parseInt(readReg1.getValue().toString(),2)));
-		readData2.setValue(getVal(Integer.parseInt(readReg2.getValue().toString(),2)));
-	//	readData1.setValue(getVal(readReg1.getValue().intValue()));
-	//	readData2.setValue(getVal(readReg2.getValue().intValue()));
+		readData1.setValue(getVal(readReg1.getValue().toLong().intValue()));
+		readData1.setValue(getVal(readReg2.getValue().toLong().intValue()));
 	}
 	
 }
