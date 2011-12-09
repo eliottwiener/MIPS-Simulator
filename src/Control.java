a713e8609a5ab320b81fb9db31c2781e56a6432c
Ryan Colombo <ryco218@yahoo.com>
2011-12-09 17:08:11 -0500

    added support for binary num class

--- src/Control.java
+++ src/Control.java
@@ -21,106 +21,106 @@ public class Control{
 	// Set the control signals depending on the opcode
 	public void setSignals(){
 		
-		String myOpcode = BinaryUtil.pad(Long.toBinaryString(opcode.getValue()), 6);
+		String myOpcode = opcode.getValue().toString();
 		// R-Type instructions
 		if(myOpcode.equals("000000")){
-			jump.setValue((long)0);
-			branch.setValue((long)0);
-			memRead.setValue((long)0);
-			memToReg.setValue((long)0);
-			memWrite.setValue((long)0);
-			aluSrc.setValue((long)0);
-			branchBNE.setValue((long)0);
-			immediate.setValue((long)0);
+			jump.setValue(new BinaryNum("0"));
+			branch.setValue(new BinaryNum("0"));
+			memRead.setValue(new BinaryNum("0"));
+			memToReg.setValue(new BinaryNum("0"));
+			memWrite.setValue(new BinaryNum("0"));
+			aluSrc.setValue(new BinaryNum("0"));
+			branchBNE.setValue(new BinaryNum("0"));
+			immediate.setValue(new BinaryNum("0"));
 					
-			String myFunct = BinaryUtil.pad(Long.toBinaryString(funct.getValue()),6);
+			String myFunct = funct.getValue().toString();
 			if(myFunct.equals("001000")){
-				regDst.setValue((long)0);
-				jumpReg.setValue((long)1);
-				aluOp.setValue((long)0);
-				regWrite.setValue((long)0);
+				regDst.setValue(new BinaryNum("0"));
+				jumpReg.setValue(new BinaryNum("1"));
+				aluOp.setValue(new BinaryNum("0"));
+				regWrite.setValue(new BinaryNum("0"));
 			}else{
-				regDst.setValue((long)1);
-				jumpReg.setValue((long)0);
-				aluOp.setValue((long)2);
-				regWrite.setValue((long)1);
+				regDst.setValue(new BinaryNum("1"));
+				jumpReg.setValue(new BinaryNum("0"));
+				aluOp.setValue(new BinaryNum("10"));
+				regWrite.setValue(new BinaryNum("1"));
 				
 			}
 		}
 		
 		// LW instruction
 		if(myOpcode.equals("100011")){
-			jump.setValue((long)0);
-			regDst.setValue((long)0);
-			branch.setValue((long)0);
-			memRead.setValue((long)1);
-			memToReg.setValue((long)1);
-			aluOp.setValue((long)0);
-			memWrite.setValue((long)0);
-			aluSrc.setValue((long)1);
-			regWrite.setValue((long)1);
-			jumpReg.setValue((long)0);
-			branchBNE.setValue((long)0);
+			jump.setValue(new BinaryNum("0"));
+			regDst.setValue(new BinaryNum("0"));
+			branch.setValue(new BinaryNum("0"));
+			memRead.setValue(new BinaryNum("1"));
+			memToReg.setValue(new BinaryNum("1"));
+			aluOp.setValue(new BinaryNum("0"));
+			memWrite.setValue(new BinaryNum("0"));
+			aluSrc.setValue(new BinaryNum("1"));
+			regWrite.setValue(new BinaryNum("1"));
+			jumpReg.setValue(new BinaryNum("0"));
+			branchBNE.setValue(new BinaryNum("0"));
 		}
 		
 		// SW instruction
 		if(myOpcode.equals("101011")){
-			jump.setValue((long)0);
-			regDst.setValue((long)0);
-			branch.setValue((long)0);
-			memRead.setValue((long)0);
-			memToReg.setValue((long)0);
-			aluOp.setValue((long)0);
-			memWrite.setValue((long)1);
-			aluSrc.setValue((long)1);
-			regWrite.setValue((long)0);
-			jumpReg.setValue((long)0);
-			branchBNE.setValue((long)0);
+			jump.setValue(new BinaryNum("0"));
+			regDst.setValue(new BinaryNum("0"));
+			branch.setValue(new BinaryNum("0"));
+			memRead.setValue(new BinaryNum("0"));
+			memToReg.setValue(new BinaryNum("0"));
+			aluOp.setValue(new BinaryNum("0"));
+			memWrite.setValue(new BinaryNum("1"));
+			aluSrc.setValue(new BinaryNum("1"));
+			regWrite.setValue(new BinaryNum("0"));
+			jumpReg.setValue(new BinaryNum("0"));
+			branchBNE.setValue(new BinaryNum("0"));
 		}
 		
 		// beq
 		if(myOpcode.equals("000100")){
-			jump.setValue((long)0);
-			regDst.setValue((long)0);
-			branch.setValue((long)1);
-			memRead.setValue((long)0);
-			memToReg.setValue((long)0);
-			aluOp.setValue((long)1);
-			memWrite.setValue((long)0);
-			aluSrc.setValue((long)0);
-			regWrite.setValue((long)0);
-			jumpReg.setValue((long)0);
-			branchBNE.setValue((long)0);
+			jump.setValue(new BinaryNum("0"));
+			regDst.setValue(new BinaryNum("0"));
+			branch.setValue(new BinaryNum("1"));
+			memRead.setValue(new BinaryNum("0"));
+			memToReg.setValue(new BinaryNum("0"));
+			aluOp.setValue(new BinaryNum("1"));
+			memWrite.setValue(new BinaryNum("0"));
+			aluSrc.setValue(new BinaryNum("0"));
+			regWrite.setValue(new BinaryNum("0"));
+			jumpReg.setValue(new BinaryNum("0"));
+			branchBNE.setValue(new BinaryNum("0"));
 		}
 		
 		// bne
 		if(myOpcode.equals("000101")){
-			jump.setValue((long)0);
-			regDst.setValue((long)0);
-			branch.setValue((long)1);
-			memRead.setValue((long)0);
-			memToReg.setValue((long)0);
-			aluOp.setValue((long)1);
-			memWrite.setValue((long)0);
-			aluSrc.setValue((long)0);
-			regWrite.setValue((long)0);
-			jumpReg.setValue((long)0);
-			branchBNE.setValue((long)1);
+			jump.setValue(new BinaryNum("0"));
+			regDst.setValue(new BinaryNum("0"));
+			branch.setValue(new BinaryNum("1"));
+			memRead.setValue(new BinaryNum("0"));
+			memToReg.setValue(new BinaryNum("0"));
+			aluOp.setValue(new BinaryNum("1"));
+			memWrite.setValue(new BinaryNum("0"));
+			aluSrc.setValue(new BinaryNum("0"));
+			regWrite.setValue(new BinaryNum("0"));
+			jumpReg.setValue(new BinaryNum("0"));
+			branchBNE.setValue(new BinaryNum("1"));
 		}
 		
 		// jump instruction
 		if(myOpcode.equals("000010")){
-			jump.setValue((long)1);
-			regDst.setValue((long)0);
-			branch.setValue((long)0);
-			memRead.setValue((long)0);
-			memToReg.setValue((long)0);
-			aluOp.setValue((long)0);
-			memWrite.setValue((long)0);
-			aluSrc.setValue((long)1);
-			regWrite.setValue((long)0);
-			jumpReg.setValue((long)0);
-			branchBNE.setValue((long)0);
+			jump.setValue(new BinaryNum("1"));
+			regDst.setValue(new BinaryNum("0"));
+			branch.setValue(new BinaryNum("0"));
+			memRead.setValue(new BinaryNum("0"));
+			memToReg.setValue(new BinaryNum("0"));
+			aluOp.setValue(new BinaryNum("0"));
+			memWrite.setValue(new BinaryNum("0"));
+			aluSrc.setValue(new BinaryNum("1"));
+			regWrite.setValue(new BinaryNum("0"));
+			jumpReg.setValue(new BinaryNum("0"));
+			branchBNE.setValue(new BinaryNum("0"));
 		}
 		
 		
@@ -130,18 +130,18 @@ public class Control{
 				myOpcode.equals("001100") ||
 				myOpcode.equals("001101") ||
 				myOpcode.equals("001010")){
-			jump.setValue((long)0);
-			regDst.setValue((long)0);
-			branch.setValue((long)0);
-			memRead.setValue((long)0);
-			memToReg.setValue((long)0);
-			aluOp.setValue((long)2);
-			memWrite.setValue((long)0);
-			aluSrc.setValue((long)1);
-			regWrite.setValue((long)1);
-			jumpReg.setValue((long)0);
-			branchBNE.setValue((long)0);
-			immediate.setValue((long)1);
+			jump.setValue(new BinaryNum("0"));
+			regDst.setValue(new BinaryNum("0"));
+			branch.setValue(new BinaryNum("0"));
+			memRead.setValue(new BinaryNum("0"));
+			memToReg.setValue(new BinaryNum("0"));
+			aluOp.setValue(new BinaryNum("10"));
+			memWrite.setValue(new BinaryNum("0"));
+			aluSrc.setValue(new BinaryNum("1"));
+			regWrite.setValue(new BinaryNum("1"));
+			jumpReg.setValue(new BinaryNum("0"));
+			branchBNE.setValue(new BinaryNum("0"));
+			immediate.setValue(new BinaryNum("1"));
 		}
 		
 	}
