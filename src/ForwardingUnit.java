d200556329165d6ad5e2729f17dbe865d8450c1f
Ryan Colombo <ryco218@yahoo.com>
2011-12-09 17:08:32 -0500

    added support for binary num class

--- src/ForwardingUnit.java
+++ src/ForwardingUnit.java
@@ -11,42 +11,42 @@ public class ForwardingUnit implements Clockable{
 	public ForwardingUnit(){}
 	
 	public void clockEdge(){
-		Long idex_rs_val = idex_rs.getValue();
-		Long idex_rt_val = idex_rt.getValue();
-		Long exmem_rd_val = exmem_rd.getValue();
-		Long exmem_regWrite_val = exmem_regWrite.getValue();
-		Long memwb_rd_val = memwb_rd.getValue();
-		Long memwb_regWrite_val = memwb_regWrite.getValue();
+		BinaryNum idex_rs_val = idex_rs.getValue();
+		BinaryNum idex_rt_val = idex_rt.getValue();
+		BinaryNum exmem_rd_val = exmem_rd.getValue();
+		BinaryNum exmem_regWrite_val = exmem_regWrite.getValue();
+		BinaryNum memwb_rd_val = memwb_rd.getValue();
+		BinaryNum memwb_regWrite_val = memwb_regWrite.getValue();
 		
 		// (page 367 & 369)
 		// EX hazard
-		if(exmem_regWrite_val.equals((long)1)
-				&& !(exmem_rd_val.equals((long)0))
-				&& exmem_rd_val.equals((long)idex_rs_val)){
-			forwardA.setValue((long)2);
-		} else forwardA.setValue((long)0);
-		if(exmem_regWrite_val.equals((long)1)
-				&& !(exmem_rd_val.equals((long)0))
-				&& exmem_rd_val.equals((long)idex_rt_val)){
-			forwardB.setValue((long)2);
-		} else forwardB.setValue((long)0);
+		if(exmem_regWrite_val.equals(new BinaryNum("1"))
+				&& !(exmem_rd_val.equals(new BinaryNum("0")))
+				&& exmem_rd_val.equals(new BinaryNum(idex_rs_val.toString()))){
+			forwardA.setValue(new BinaryNum("10"));
+		} else forwardA.setValue(new BinaryNum("0"));
+		if(exmem_regWrite_val.equals(new BinaryNum("1"))
+				&& !(exmem_rd_val.equals(new BinaryNum("0")))
+				&& exmem_rd_val.equals(new BinaryNum(idex_rt_val.toString()))){
+			forwardB.setValue(new BinaryNum("10"));
+		} else forwardB.setValue(new BinaryNum("0"));
 		
 		// MEM hazard
-		if(memwb_regWrite_val.equals((long)1)
-				&& !(memwb_rd_val.equals((long)0))
-				&& !(exmem_regWrite_val.equals((long)1)
-						&& !(exmem_rd_val.equals((long)0))
-						&& !(exmem_rd_val.equals((long)idex_rs_val)))
-				&& memwb_rd_val.equals((long)idex_rs_val)){
-			forwardA.setValue((long)1);
-		} else forwardA.setValue((long)0);
-		if(memwb_regWrite_val.equals((long)1)
-				&& !(memwb_rd_val.equals((long)0))
-				&& !(exmem_regWrite_val.equals((long)1)
-						&& !(exmem_rd_val.equals((long)0))
-						&& !(exmem_rd_val.equals((long)idex_rt_val)))
-				&& memwb_rd_val.equals((long)idex_rt_val)){
-			forwardB.setValue((long)1);
-		} else forwardB.setValue((long)0);
+		if(memwb_regWrite_val.equals(new BinaryNum("1"))
+				&& !(memwb_rd_val.equals(new BinaryNum("0")))
+				&& !(exmem_regWrite_val.equals(new BinaryNum("1"))
+						&& !(exmem_rd_val.equals(new BinaryNum("0")))
+						&& !(exmem_rd_val.equals(new BinaryNum(idex_rs_val.toString()))))
+				&& memwb_rd_val.equals(new BinaryNum(idex_rs_val.toString()))){
+			forwardA.setValue(new BinaryNum("1"));
+		} else forwardA.setValue(new BinaryNum("0"));
+		if(memwb_regWrite_val.equals(new BinaryNum("1"))
+				&& !(memwb_rd_val.equals(new BinaryNum("0")))
+				&& !(exmem_regWrite_val.equals(new BinaryNum("1"))
+						&& !(exmem_rd_val.equals(new BinaryNum("0")))
+						&& !(exmem_rd_val.equals(new BinaryNum(idex_rt_val.toString()))))
+				&& memwb_rd_val.equals(new BinaryNum(idex_rt_val.toString()))){
+			forwardB.setValue(new BinaryNum("1"));
+		} else forwardB.setValue(new BinaryNum("0"));
 	}
 }
