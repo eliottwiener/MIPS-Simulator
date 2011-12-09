e7604d3a2f9a9e5608a06dd95e61c50ecb60c32c
Ryan Colombo <ryco218@yahoo.com>
2011-12-09 17:08:58 -0500

    added support for binary num class

--- src/ProgramReader.java
+++ src/ProgramReader.java
@@ -9,16 +9,19 @@ import java.util.List;
 
 
 public class ProgramReader {
-	public static List<Long> getMemory(String fileName) {
+	public static List<BinaryNum> getMemory(String fileName) {
 		File file = new File(fileName);
 		DataInputStream fin = null;
-		List<Long> individualBytes = new ArrayList<Long>();
+		List<BinaryNum> individualBytes = new ArrayList<BinaryNum>();
 		try
 		{
 			fin = new DataInputStream(new FileInputStream(file));
 			while(true){
 				int b = fin.readUnsignedByte();
-				individualBytes.add(new Long(b));
+				String s = Integer.toBinaryString(b);
+				BinaryNum bn = new BinaryNum(s);
+				bn = bn.pad(8);
+				individualBytes.add(bn);
 			}
 		} catch(EOFException e){
 		}
