7e3aa4ef2fb3c5877de9301718701156b7f75bf1
Ryan Colombo <ryco218@yahoo.com>
2011-12-09 14:14:43 -0500

    added Mux3 class

---
+++ src/Mux3.java
@@ -0,0 +1,22 @@
+public class Mux3 implements Clockable{
+	
+	public final Pin output = new Pin(); 	
+	public final Pin switcher = new Pin();
+	public final Pin input0 = new Pin();
+	public final Pin input1 = new Pin();
+	public final Pin input2 = new Pin();
+	
+	Mux3(){}
+	
+	public void clockEdge() {
+		if(switcher.getValue().equals((long)0)){
+			output.setValue(input0.getValue());
+		}
+		else if(switcher.getValue().equals((long)1)){
+			output.setValue(input1.getValue());
+		}
+		else if(switcher.getValue().equals((long)2)){
+			output.setValue(input2.getValue());
+		}
+	}
+}
