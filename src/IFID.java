d806b514d6b66fab1d6b80d394830d19e0d48bab
Ryan Colombo <ryco218@yahoo.com>
2011-12-10 16:32:18 -0500

    added flush

--- src/IFID.java
+++ src/IFID.java
@@ -3,6 +3,7 @@ public class IFID implements Clockable{
 	public Pin IFIDWrite = new Pin();
 	public Pin PC4 = new Pin();
 	public Pin instruction = new Pin();
+	public Pin Flush = new Pin();
 	//outputs
 	public Pin outPC4 = new Pin();
 	public Pin outInstr = new Pin();
@@ -10,10 +11,14 @@ public class IFID implements Clockable{
 	public IFID(){
 		outPC4.setValue(new BinaryNum("0").pad(32));
 		outInstr.setValue(new BinaryNum("0").pad(32));
+		Flush.setValue(new BinaryNum("0"));
 	}
 	
 	public void clockEdge(){
-		if(new BinaryNum("0").equals(IFIDWrite.getValue())){
+		if(new BinaryNum("1").equals(Flush.getValue())){
+			outInstr.setValue(new BinaryNum("0").pad(32));
+		}
+		else if(new BinaryNum("0").equals(IFIDWrite.getValue())){
 			outPC4.setValue(PC4.getValue());
 			outInstr.setValue(instruction.getValue());
 		}
