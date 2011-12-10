c8d4a29d160ab3091c814d37c0facb68d0c139db
Ryan Colombo <ryco218@yahoo.com>
2011-12-10 15:02:58 -0500

    added IFIDWrite control from HDU

--- src/IFID.java
+++ src/IFID.java
@@ -1,6 +1,6 @@
 public class IFID implements Clockable{
 	// inputs
-	public Pin IFDWrite = new Pin();
+	public Pin IFIDWrite = new Pin();
 	public Pin PC4 = new Pin();
 	public Pin instruction = new Pin();
 	//outputs
@@ -11,8 +11,10 @@ public class IFID implements Clockable{
 	}
 	
 	public void clockEdge(){
-		outPC4.setValue(PC4.getValue());
-		outInstr.setValue(instruction.getValue());
+		if(new BinaryNum("0").equals(IFIDWrite.getValue())){
+			outPC4.setValue(PC4.getValue());
+			outInstr.setValue(instruction.getValue());
+		}
 	}
 	
 }
