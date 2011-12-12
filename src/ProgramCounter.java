e0e9597e79f7cc9ba3a83524c8778069673a31e8
Ryan Colombo <ryco218@yahoo.com>
2011-12-11 22:58:13 -0500

    added instruction count

--- src/ProgramCounter.java
+++ src/ProgramCounter.java
@@ -3,6 +3,7 @@ public class ProgramCounter implements Clockable{
 	Pin pcIn = new Pin();
 	Pin pcOut = new Pin();
 	Pin control = new Pin();
+	int instrCount = 0;
 	
 	public ProgramCounter(){
 		pcIn.setValue(new BinaryNum("0").pad(32));
@@ -12,6 +13,7 @@ public class ProgramCounter implements Clockable{
 
 	public void clockEdge() {
 		if(new BinaryNum("0").equals(control.getValue())){
+			instrCount++;
 			pcOut.setValue(pcIn.getValue());
 		}
 	}
